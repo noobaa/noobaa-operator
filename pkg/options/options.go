5894fd672c2ec9a74837920006f9c7a7cd04a341
Danny Zaken <dannyzaken@gmail.com>
2020-03-24 11:32:38 +0200

    updated core and operator versions for 2.1.0. release

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -30,7 +30,7 @@ const (
 	// ContainerImageRepo is the repo of the default image url
 	ContainerImageRepo = "noobaa-core"
 	// ContainerImageTag is the tag of the default image url
-	ContainerImageTag = "5.3.0-master20200209"
+	ContainerImageTag = "5.3.1"
 	// ContainerImageSemverLowerBound is the lower bound for supported image versions
 	ContainerImageSemverLowerBound = "5.0.0"
 	// ContainerImageSemverUpperBound is the upper bound for supported image versions
