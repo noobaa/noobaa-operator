6494c42c043e7e42fd6bf07d69897ea1c4e2c274
nb-ohad <mitrani.ohad@gmail.com>
2020-07-27 13:16:56 +0300

    Add predicates to filter out non-relevant events and log relevant events
    Also remove manager SyncPeriod time dev code left by accident in the code
    
    Signed-off-by: nb-ohad <mitrani.ohad@gmail.com>

--- pkg/controller/bucketclass/bucketclass_controller.go
+++ pkg/controller/bucketclass/bucketclass_controller.go
@@ -3,10 +3,13 @@ package bucketclass
 import (
 	nbv1 "github.com/noobaa/noobaa-operator/v2/pkg/apis/noobaa/v1alpha1"
 	"github.com/noobaa/noobaa-operator/v2/pkg/bucketclass"
+	"github.com/noobaa/noobaa-operator/v2/pkg/util"
+
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
@@ -33,21 +36,33 @@ func Add(mgr manager.Manager) error {
 		return err
 	}
 
-	// Watch for changes on resources to trigger reconcile
+	// Predicate that allow us to log event that are being queued
+	logEventsPredicate := util.LogEventsPredicate{}
 
-	err = c.Watch(&source.Kind{Type: &nbv1.BucketClass{}}, &handler.EnqueueRequestForObject{})
+	// Predicate that allows events that only change spec, labels or finalizers and will log any allowed events
+	// This will stop infinite reconciles that triggered by status or irrelevant metadata changes
+	bucketClassPredicate := util.ComposePredicates(
+		predicate.GenerationChangedPredicate{},
+		util.LabelsChangedPredicate{},
+		util.FinalizersChangedPredicate{},
+	)
+
+	// Watch for changes on resources to trigger reconcile
+	err = c.Watch(&source.Kind{Type: &nbv1.BucketClass{}}, &handler.EnqueueRequestForObject{},
+		bucketClassPredicate, &logEventsPredicate)
 	if err != nil {
 		return err
 	}
 
-	err = c.Watch(&source.Kind{Type: &nbv1.BackingStore{}}, &handler.EnqueueRequestsFromMapFunc{
+	backingStoreHandler := handler.EnqueueRequestsFromMapFunc{
 		ToRequests: handler.ToRequestsFunc(func(obj handler.MapObject) []reconcile.Request {
 			return bucketclass.MapBackingstoreToBucketclasses(types.NamespacedName{
 				Name:      obj.Meta.GetName(),
 				Namespace: obj.Meta.GetNamespace(),
 			})
 		}),
-	})
+	}
+	err = c.Watch(&source.Kind{Type: &nbv1.BackingStore{}}, &backingStoreHandler, logEventsPredicate)
 	if err != nil {
 		return err
 	}
