42a4524c576349df426c8092d56af3553639dd71
liranmauda <liran.mauda@gmail.com>
2021-12-16 11:08:43 +0200

    Moved configmap annotation
    Signed-off-by: liranmauda <liran.mauda@gmail.com>
    (cherry picked from commit 5174fef3f7bdf79a08c8f26a67a33bba188f69ff)
    Signed-off-by: Nimrod Becker <bnimrod@gmail.com>

--- pkg/bundle/deploy.go
+++ pkg/bundle/deploy.go
@@ -2646,7 +2646,7 @@ data:
           su postgres -c "bash -x /usr/bin/run-postgresql"
 `
 
-const Sha256_deploy_internal_deployment_endpoint_yaml = "9e67a262b53e9b336a0d2810d3bf47b373704796ba7007b637492a3fac549ee7"
+const Sha256_deploy_internal_deployment_endpoint_yaml = "d098321ca3038440e35b27277c1e38eedea852a5be25371e29a442d5f7987590"
 
 const File_deploy_internal_deployment_endpoint_yaml = `apiVersion: apps/v1
 kind: Deployment
@@ -2654,8 +2654,6 @@ metadata:
   labels:
     app: noobaa
   name: noobaa-endpoint
-  annotations:
-    ConfigMapHash: ""
 spec:
   replicas: 1
   selector:
@@ -2671,6 +2669,8 @@ spec:
       labels:
         noobaa-s3: noobaa
         app: noobaa
+      annotations:
+        noobaa.io/configmap-hash: ""
     spec:
       serviceAccountName: noobaa-endpoint
       volumes:
@@ -3268,7 +3268,7 @@ spec:
       noobaa-s3-svc: "true"
 `
 
-const Sha256_deploy_internal_statefulset_core_yaml = "002ed83aac0df2458a67fe77ce70d2028845a415f5fc49ceaaa007ab159a2e3b"
+const Sha256_deploy_internal_statefulset_core_yaml = "f7bf9089a492b34c463bd92f621c19ba6908c660518b71b90f1db5b2ab3ee055"
 
 const File_deploy_internal_statefulset_core_yaml = `apiVersion: apps/v1
 kind: StatefulSet
@@ -3276,8 +3276,6 @@ metadata:
   name: noobaa-core
   labels:
     app: noobaa
-  annotations:
-    ConfigMapHash: ""
 spec:
   replicas: 1
   selector:
@@ -3292,6 +3290,8 @@ spec:
         app: noobaa
         noobaa-core: noobaa
         noobaa-mgmt: noobaa
+      annotations:
+        noobaa.io/configmap-hash: ""
     spec:
       serviceAccountName: noobaa
       volumes:
