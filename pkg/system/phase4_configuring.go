be74421cf49e49a8676a7fdac81ce8d30bf19761
jackyalbo <jacky.albo@gmail.com>
2022-08-31 09:26:45 +0300

    support OCP injected CA
    Signed-off-by: jackyalbo <jalbo@redhat.com>
    (cherry picked from commit f193d70b5d98fdc73a0d67da9ef797121409f6d5)
    Signed-off-by: Nimrod Becker <bnimrod@gmail.com>

--- pkg/system/phase4_configuring.go
+++ pkg/system/phase4_configuring.go
@@ -415,6 +415,30 @@ func (r *Reconciler) setDesiredEndpointMounts(podSpec *corev1.PodSpec, container
 	podSpec.Volumes = r.DefaultDeploymentEndpoint.Volumes
 	container.VolumeMounts = r.DefaultDeploymentEndpoint.Containers[0].VolumeMounts
 
+	if util.KubeCheckQuiet(r.CaBundleConf) {
+		configMapVolumes := []corev1.Volume {{
+			Name: r.CaBundleConf.Name,
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: r.CaBundleConf.Name,
+					},
+					Items: []corev1.KeyToPath{{
+						Key:  "ca-bundle.crt",
+						Path: "tls-ca-bundle.pem",
+					}},
+				},
+			},
+		}}
+		util.MergeVolumeList(&podSpec.Volumes, &configMapVolumes)
+		configMapVolumeMounts := []corev1.VolumeMount {{
+			Name:      r.CaBundleConf.Name,
+			MountPath: "/etc/pki/ca-trust/extracted/pem",
+			ReadOnly: true,
+		}}
+		util.MergeVolumeMountList(&container.VolumeMounts, &configMapVolumeMounts)
+	}
+
 	for _, nsStore := range namespaceStoreList.Items {
 		// Since namespacestore is able to get a rejected state on runtime errors,
 		// we want to skip namespacestores with invalid configuration only.
