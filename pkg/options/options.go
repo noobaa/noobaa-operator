b8d2bbb6a212eaa8bfd12cd9e683a22e10225851
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-05-01 13:50:18 +0530

    fix 5.12 CI
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -30,7 +30,7 @@ const (
 	// ContainerImageRepo is the repo of the default image url
 	ContainerImageRepo = "noobaa-core"
 	// ContainerImageTag is the tag of the default image url
-	ContainerImageTag = "master-20220913"
+	ContainerImageTag = "5.12-20230424"
 	// ContainerImageSemverLowerBound is the lower bound for supported image versions
 	ContainerImageSemverLowerBound = "5.0.0"
 	// ContainerImageSemverUpperBound is the upper bound for supported image versions
