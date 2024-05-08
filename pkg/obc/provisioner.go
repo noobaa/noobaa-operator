5f8ed8d616d4eac8eba443b5a8491d8ada48b4e5
Ben <belimele@redhat.com>
2024-05-08 21:58:33 +0200

    Lint
    Signed-off-by: Ben <belimele@redhat.com>

--- pkg/obc/provisioner.go
+++ pkg/obc/provisioner.go
@@ -580,8 +580,8 @@ func (r *BucketRequest) CreateAccount() error {
 		nsfsAccountConfig.NsfsOnly = true;
 		// -1 is the default CLI value which we use to indicate that the UID/GID should not be set
 		// 0 cannot be used since it is a valid GID/UID value
-		var IdNullifier = -1
-		if nsfsAccountConfig.UID == &IdNullifier {
+		var IDNullifier = -1
+		if nsfsAccountConfig.UID == &IDNullifier {
 			nsfsAccountConfig.UID = nil
 			nsfsAccountConfig.GID = nil
 		}
