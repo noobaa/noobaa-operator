418e2bd260038d382f2eb758d087aece12278b8e
Ben <belimele@redhat.com>
2024-05-15 11:00:37 +0200

    Fix CLI tests
    Signed-off-by: Ben <belimele@redhat.com>

--- test/cli/test_cli_functions.sh
+++ test/cli/test_cli_functions.sh
@@ -1404,21 +1404,23 @@ EOF
     echo_time "✅  [${FUNCNAME[0]}]: Noobaa multinamespace bucketclass test passed"
 }
 
+# Should fail due to:
 function obc_nsfs_negative_tests {
-    # Should fail due to:
-        # No GID
+    # No GID
     yes n | test_noobaa should_fail obc create testobc --uid 42
-        # No UID
+    # No UID
     yes n | test_noobaa should_fail obc create testobc --gid 505
-        # Distingusihed name provided in conjunction with UID
+    # Distingusihed name provided in conjunction with UID
     yes n | test_noobaa should_fail obc create testobc --distinguished-name 'test' --uid 42
+    # GID and UID (valid) but no bucketclass
+    yes n | test_noobaa should_fail obc create testobc --uid 42 --gid 505
 }
 
 function test_create_obc_with_nsfs_acc_cfg_uid_gid_logic {
     local uid=$1
     local gid=$2
     local obc_name="giduidtestobc"
-    test_noobaa obc create ${obc_name} --uid ${uid} --gid ${gid}
+    test_noobaa obc create ${obc_name} --uid ${uid} --gid ${gid} --bucketclass noobaa-default-backing-store
     local obc_account_nsfs_account=$(test_noobaa api account list_accounts {} -ojson | jq '.accounts[] | select(.bucket_claim_owner | test("'${obc_name}'"))?' | jq '.nsfs_account_config')
     local account_gid=$(echo $obc_account_nsfs_account | jq '.gid')
     local account_uid=$(echo $obc_account_nsfs_account | jq '.uid')
@@ -1441,7 +1443,7 @@ function test_create_obc_with_nsfs_acc_cfg_uid_gid {
 function test_create_obc_with_nsfs_acc_distinguished_name {
     local distinguished_name="someuser"
     local obc_name="distinguishednameobc"
-    test_noobaa obc create ${obc_name} --distinguished-name $distinguished_name
+    test_noobaa obc create ${obc_name} --distinguished-name $distinguished_name --bucketclass noobaa-default-backing-store
     local obc_account_nsfs_account=$(test_noobaa api account list_accounts {} -ojson | jq '.accounts[] | select(.bucket_claim_owner | test("'${obc_name}'"))?' | jq '.nsfs_account_config')
     local account_distinguished_name=$(echo $obc_account_nsfs_account | jq -r '.distinguished_name')
     test_noobaa obc delete ${obc_name}
