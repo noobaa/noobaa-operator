cddbdee686d717165f9046aa4f4605f85ee2a1d4
Ben <belimele@redhat.com>
2025-05-20 12:44:43 +0200

    Fix leftover if
    Signed-off-by: Ben <belimele@redhat.com>

--- pkg/system/phase4_configuring.go
+++ pkg/system/phase4_configuring.go
@@ -1371,7 +1371,7 @@ func (r *Reconciler) prepareCephBackingStore() error {
 		Transport: util.InsecureHTTPTransport,
 		Timeout:   10 * time.Second,
 	}
-	if r.UseRefreshingTransport {
+	if r.UserCertBundlePath != "" {
 		client.Transport = util.GlobalCARefreshingTransport
 	}
 
