25020d48661d36052e2c2609b14b0a6f64889f21
shirady <57721533+shirady@users.noreply.github.com>
2025-05-26 09:23:54 +0200

    in the core and the endpoint change the mountpath - remove the extension
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>
    Signed-off-by: Ben <belimele@redhat.com>

--- pkg/system/phase2_creating.go
+++ pkg/system/phase2_creating.go
@@ -578,7 +578,7 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 			if util.KubeCheckQuiet(r.CaBundleConf) {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
@@ -661,7 +661,7 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 			if util.KubeCheckQuiet(r.CaBundleConf) {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
