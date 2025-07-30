492956e19d21ae0685787ac0921eb27aad4ed31b
Ben <belimele@redhat.com>
2025-07-31 00:26:08 +0200

    Operator certificate handling refactor
    - Move the ServiceServingCertCAFile var from options.go to util.go
    - Fix the MountPath of the OCp injected CA bundle and improve handling of the configmap
    - Readd the global refreshing transport to the S3 clients in prepareAWSBackingStore
    - Rename AddToRootCAs to CombineCaBundle for added clarity
    - Add an explanation about the current certificate flow and its implication on other pods, and their HTTP agents
    
    Signed-off-by: Ben <belimele@redhat.com>
    (cherry picked from commit e1260ff9bcc609232e18859ab5acf4f058ea4a91)

--- pkg/system/phase2_creating.go
+++ pkg/system/phase2_creating.go
@@ -575,10 +575,11 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 				util.MergeVolumeMountList(&c.VolumeMounts, &dbSecretVolumeMounts)
 			}
 
-			if util.KubeCheckQuiet(r.CaBundleConf) {
+			// we want to check that the cm exists and also that it has data in it
+			if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
@@ -658,10 +659,11 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 					Limits:   logResourceList,
 				}
 			}
-			if util.KubeCheckQuiet(r.CaBundleConf) {
+			// we want to check that the cm exists and also that it has data in it
+			if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
@@ -701,7 +703,8 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 
 	r.CoreApp.Spec.Template.Annotations["noobaa.io/configmap-hash"] = r.CoreAppConfig.Annotations["noobaa.io/configmap-hash"]
 
-	if util.KubeCheckQuiet(r.CaBundleConf) {
+	// we want to check that the cm exists and also that it has data in it
+	if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 		configMapVolumes := []corev1.Volume{{
 			Name: r.CaBundleConf.Name,
 			VolumeSource: corev1.VolumeSource{
