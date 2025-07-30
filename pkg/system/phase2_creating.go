a9beff338ea2f1e31e273babe9fc550e3ba13905
Ben <belimele@redhat.com>
2025-07-31 00:26:08 +0200

    Operator certificate handling refactor
    - Move the ServiceServingCertCAFile var from options.go to util.go
    - Fix the MountPath of the OCp injected CA bundle and improve handling of the configmap
    - Readd the global refreshing transport to the S3 clients in prepareAWSBackingStore
    - Rename AddToRootCAs to CombineCaBundle for added clarity
    - Add an explanation about the current certificate flow and its implication on other pods, and their HTTP agents
    
    Signed-off-by: Ben <belimele@redhat.com>
    (cherry picked from commit e1260ff9bcc609232e18859ab5acf4f058ea4a91)
    (cherry picked from commit 492956e19d21ae0685787ac0921eb27aad4ed31b)

--- pkg/system/phase2_creating.go
+++ pkg/system/phase2_creating.go
@@ -527,10 +527,12 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 			if r.NooBaa.Spec.CoreResources != nil {
 				c.Resources = *r.NooBaa.Spec.CoreResources
 			}
-			if util.KubeCheckQuiet(r.CaBundleConf) {
+
+			// we want to check that the cm exists and also that it has data in it
+			if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
@@ -610,10 +612,11 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 					Limits:   logResourceList,
 				}
 			}
-			if util.KubeCheckQuiet(r.CaBundleConf) {
+			// we want to check that the cm exists and also that it has data in it
+			if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 				configMapVolumeMounts := []corev1.VolumeMount{{
 					Name:      r.CaBundleConf.Name,
-					MountPath: "/etc/ocp-injected-ca-bundle.crt",
+					MountPath: "/etc/ocp-injected-ca-bundle",
 					ReadOnly:  true,
 				}}
 				util.MergeVolumeMountList(&c.VolumeMounts, &configMapVolumeMounts)
@@ -653,7 +656,8 @@ func (r *Reconciler) SetDesiredCoreApp() error {
 
 	r.CoreApp.Spec.Template.Annotations["noobaa.io/configmap-hash"] = r.CoreAppConfig.Annotations["noobaa.io/configmap-hash"]
 
-	if util.KubeCheckQuiet(r.CaBundleConf) {
+	// we want to check that the cm exists and also that it has data in it
+	if util.KubeCheckQuiet(r.CaBundleConf) && len(r.CaBundleConf.Data) > 0 {
 		configMapVolumes := []corev1.Volume{{
 			Name: r.CaBundleConf.Name,
 			VolumeSource: corev1.VolumeSource{
