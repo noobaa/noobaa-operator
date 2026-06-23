b969d61e7931dd56923f38b63bd6a6ac8b910459
Danny Zaken <dannyzaken@gmail.com>
2026-06-23 10:56:15 -0400

    Bumped cnpg dependency to 1.29.1
    - pinned controller-runtime to v0.22.4
    - pinned k8s.io deps to v0.35.3
    
    Signed-off-by: Danny Zaken <dannyzaken@gmail.com>

--- pkg/cosi/cosi_bucket_claim.go
+++ pkg/cosi/cosi_bucket_claim.go
@@ -295,7 +295,7 @@ func WaitReady(cosiBucketClaim *nbv1.COSIBucketClaim) bool {
 	retries := 0
 	err := wait.PollUntilContextCancel(ctx, interval*time.Second, true, func(ctx context.Context) (bool, error) {
 		if retries == maxRetries {
-			return false, fmt.Errorf("COSI bucket claim is not ready after max retries - %q", maxRetries)
+			return false, fmt.Errorf("COSI bucket claim is not ready after max retries - %d", maxRetries)
 		}
 		retries++
 		err := klient.Get(util.Context(), util.ObjectKey(cosiBucketClaim), cosiBucketClaim)
